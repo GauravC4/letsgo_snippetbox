e9ee212a671880aefc94f38d78cc72492c23800f
Gaurav <gaurav@medflix.app>
2024-07-30 21:14:09 +0530

    post method validation

--- main.go
+++ main.go
@@ -18,6 +18,11 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 }
 
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", "POST")
+		http.Error(w, "Invalid Method", http.StatusMethodNotAllowed)
+		return
+	}
 	w.Write([]byte("Creating a snippet"))
 }
 
