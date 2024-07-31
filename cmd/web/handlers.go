80d3ea1855c02895d011505d56b8a5e92e19b086
Gaurav <gaurav@medflix.app>
2024-07-31 22:29:09 +0530

    folder structure

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"strconv"
 )
@@ -12,36 +11,21 @@ func home(w http.ResponseWriter, r *http.Request) {
 		http.NotFound(w, r)
 		return
 	}
-	w.Write([]byte("Hello world!"))
+	w.Write([]byte("Hello from Snippetbox"))
 }
-
 func snippetView(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
 		http.NotFound(w, r)
 		return
 	}
-	//w.Write([]byte("Viewing a snippet"))
 	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 }
-
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
-		w.Header().Set("Allow", "POST")
-		http.Error(w, "Invalid Method", http.StatusMethodNotAllowed)
+		w.Header().Set("Allow", http.MethodPost)
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
 		return
 	}
-	w.Write([]byte("Creating a snippet"))
-}
-
-func main() {
-	mux := http.NewServeMux()
-
-	mux.HandleFunc("/snippet/view", snippetView)
-	mux.HandleFunc("/snippet/create", snippetCreate)
-	mux.HandleFunc("/", home)
-
-	log.Print("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
+	w.Write([]byte("Create a new snippet..."))
 }
